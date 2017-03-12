c6f197c51719c966f4ae853cf71715736c7cf6dd
John Messina <johnvm914@gmail.com>
2017-03-12 11:59:29 -0400

    added more login code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= Unirest.get("http://localhost:3000/resume_api",
+                        parameters: {id: session[:user_id]}
+                      ).body
+  end
+  helper_method :current_user
+
+  def authenticate_user!
+    redirect_to "/login" unless current_user
+  end
+
 end
